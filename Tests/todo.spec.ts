ca5388db9ade82b912ddb23c32ba54ce1b92310a
Hoang-Ngoc-Anh <hoangngocanh288@gmail.com>
2023-08-26 15:29:56 +0700

    edit yml file and add 1 test

--- Tests/todo.spec.ts
+++ Tests/todo.spec.ts
@@ -3,6 +3,7 @@ import User from '../Models/User';
 import SignupPage from '../pages/SignupPage';
 import TodoPage from '../pages/TodoPage';
 import NewToDoPage from '../pages/NewTodoPage';
+import ToDoApi from '../apis/TodoApi';
 
 test("Should be able to add a new todo", async ({ page, request, context }) => {
 
@@ -23,19 +24,19 @@ test("Should be able to add a new todo", async ({ page, request, context }) => {
 
 });
 
-// test("Should be able to delete a todo", async ({ page, request, context }) => {
-//     const user = new User();
-//     const signupPage = new SignupPage();
-//     await signupPage.signupUsingAPI(request, user, context);
-//     await new ToDoApi().addToDo(request, user);
+test("Should be able to delete a todo", async ({ page, request, context }) => {
+    const user = new User();
+    const signupPage = new SignupPage();
+    await signupPage.signupUsingAPI(request, user, context);
+    await new ToDoApi().addToDo(request, user);
 
-//     const todoPage = new TodoPage();
-//     await todoPage.load(page);
-//     await todoPage.deleteTodo(page)
+    const todoPage = new TodoPage();
+    await todoPage.load(page);
+    await todoPage.deleteTodo(page)
 
-//     const noToDoMessage = await todoPage.getNoTodoMessage(page);
-//     await expect(noToDoMessage).toBeVisible();
-//     expect(await noToDoMessage.innerText()).toEqual("No Available Todos");
+    const noToDoMessage = await todoPage.getNoTodoMessage(page);
+    await expect(noToDoMessage).toBeVisible();
+    expect(await noToDoMessage.innerText()).toEqual("No Available Todos");
 
-// });
+});
 
